67b5d2e604ccc3260b8d9592f375703e58e1e913
ZhukDima <zhukdo@gmail.com>
2022-11-27 23:52:32 +0300

    nice 2

--- utils/parseHash.ts
+++ utils/parseHash.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
 export const parseHash = () => window.location.hash.slice(1).split('#').map((e) => e.split(':'))
 	// eslint-disable-next-line no-return-assign, no-sequences
-	.reduce<Record<string, string>>((accum, [key, value]) => (accum[key] = value, accum), {});
+	.reduce<Record<string, string>>((accum, [key, value]) => (accum[key] = value || '1', accum), {});
