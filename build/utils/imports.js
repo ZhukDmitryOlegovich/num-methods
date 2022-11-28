abb3b95a305e2ad9968842b5ebb7133e30b40647
ZhukDima <zhukdo@gmail.com>
2022-11-28 22:45:27 +0300

    fix imports

--- build/utils/imports.js
+++ build/utils/imports.js
@@ -1,6 +1,10 @@
+const cashe = {};
 /* eslint-disable import/prefer-default-export */
-export const imports = (a) => fetch(a, {
-    headers: {
-        Accept: 'application/json',
-    },
-}).then((e) => e.json());
+export const imports = (a) => {
+    cashe[a] ?? (cashe[a] = fetch(a, {
+        headers: {
+            Accept: 'application/json',
+        },
+    }).then((e) => e.json()));
+    return cashe[a];
+};
--- utils/imports.ts
+++ utils/imports.ts
@@ -1,6 +1,11 @@
+const cashe: Record<string, Promise<[number, number, number][]>> = {};
+
 /* eslint-disable import/prefer-default-export */
-export const imports = (a: string) => fetch(a, {
-	headers: {
-		Accept: 'application/json',
-	},
-}).then((e) => e.json()) as Promise<[number, number, number][]>;
+export const imports = (a: string) => {
+	cashe[a] ??= fetch(a, {
+		headers: {
+			Accept: 'application/json',
+		},
+	}).then((e) => e.json()) as Promise<[number, number, number][]>;
+	return cashe[a];
+};
